f15e026dbb2213e9bac512995593c68eab764e05
2xki <domagoj.schreiner@gmail.com>
2019-06-17 00:38:29 +0200

    added TTS and bluetooth device list comments

--- app/src/main/java/com/example/bspeech/MainActivity.java
+++ app/src/main/java/com/example/bspeech/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.bspeech;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Set;
+import java.util.UUID;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,7 +33,18 @@ public class MainActivity extends Activity {
 
     private TextView txtSpeechInput;
     private ImageButton btnSpeak;
+    private ListView lstPairedDevices;
+    private TextToSpeech tts;
+    /*
+    private String device_address = "";
+    private String device_name = "";
+
+    private BluetoothAdapter btAdapter = null;
+    private Set<BluetoothDevice> pairedDevices;
+    */
+
     private final int REQ_CODE_SPEECH_INPUT = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +53,47 @@ public class MainActivity extends Activity {
 
         txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        lstPairedDevices = (ListView) findViewById(R.id.lstPairedDevices);
 
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR){
+                    tts.setLanguage(Locale.US);
+                }
+            }
+        });
+
+        /*btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(btAdapter == null)
+        {
+            //Show a mensag. that thedevice has no bluetooth adapter
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            //finish apk
+            finish();
+        }
+        else
+        {
+            if (btAdapter.isEnabled())
+            { }
+            else
+            {
+                //Ask to the user turn the bluetooth on
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon,1);
+            }
+        }*/
 
         btnSpeak.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                promptSpeechInput();
+                //if(device_address != ""){
+                    promptSpeechInput();
+                //}else{
+                //    listPairedDevices();
+                //}
             }
         });
 
@@ -71,6 +132,8 @@ public class MainActivity extends Activity {
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     txtSpeechInput.setText(result.get(0));
+
+                    tts.speak(result.get(0), TextToSpeech.QUEUE_FLUSH, null);
                 }
                 break;
             }
@@ -78,4 +141,33 @@ public class MainActivity extends Activity {
         }
     }
 
+    /*private void listPairedDevices()
+    {
+        pairedDevices = btAdapter.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if (pairedDevices.size()>0)
+        {
+            for(BluetoothDevice bt : pairedDevices)
+            {
+                list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        lstPairedDevices.setAdapter(adapter);
+        lstPairedDevices.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+    }
+
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            device_name = ((TextView) view).getText().toString();
+            device_address = device_name.substring(device_name.length() - 17);
+        }
+    };*/
 }
